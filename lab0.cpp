26e7d9d50e2f2fe770e564c05afc43f74623d76e
Zhukov Vlad <zukovv200@gmail.com>
2026-09-23 20:13:46 +0500

    first commit

---
+++ lab0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 5;
+
+    // Проверяем, равны ли переменные a и b
+    if (a == b) {
+        cout << "Числа равны!" << endl;
+    } else {
+        cout << "Числа не равны!" << endl;
+    }
+
+    return 0;
+}
